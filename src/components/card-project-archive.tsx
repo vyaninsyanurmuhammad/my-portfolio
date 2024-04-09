8df279c8cf073eab39d33b843d36b075819fcc56
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-04-09 22:18:00 +0700

    refactor: :iphone: add resopnsive mobile view for projects card

--- src/components/card-project-archive.tsx
+++ src/components/card-project-archive.tsx
@@ -10,9 +10,9 @@ const CardProjectArchive = ({
 }) => {
   return (
     <>
-      <div className="flex flex-row gap-6 group relative hover:!opacity-100 group-hover/list:opacity-50 transition-all ease-in-out">
+      <div className="flex flex-col sm:flex-row gap-6 group relative hover:!opacity-100 group-hover/list:opacity-50 transition-all ease-in-out">
         <div className="opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-200 bg-lime-400/10 backdrop-blur-md z-0 absolute -inset-4 rounded-xl"></div>
-        <div className="bg-lime-400 h-28 z-10 w-28 shrink-0 rounded-lg"></div>
+        <div className="bg-lime-400 h-28 z-10 w-full sm:w-28 shrink-0 rounded-lg"></div>
         <div className="flex flex-col z-10 gap-4">
           <div className="flex flex-row gap-3 items-end group-hover:items-start transition-all ease-in-out">
             <h4 className="text-white font-medium group-hover:text-lime-400">
